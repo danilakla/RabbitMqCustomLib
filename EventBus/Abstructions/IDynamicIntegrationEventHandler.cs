23b8028b32dedcc1122cb0133c209e569d45932a
danila <danila.kla@mail.ru>
2023-04-03 14:16:27 +0300

    EventBus service

---
+++ EventBus/Abstructions/IDynamicIntegrationEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Abstructions
+{
+    public interface IDynamicIntegrationEventHandler
+    {
+        Task Handle(dynamic eventData);
+    }
+
+}
